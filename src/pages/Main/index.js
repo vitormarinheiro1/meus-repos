9be2ff2b819061928ee118307c70b2f5bd53b4bd
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-05-31 13:34:21 -0300

    Adicionando função de adicionar e deletar

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useCallback } from 'react';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from '../../styles';
+import { FaBars, FaGithub, FaPlus, FaSpinner, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from '../../styles';
 
 import api from '../../services/api';
 
 export default function Main() {
 
     const [newRepo, setNewRepo] = useState('');
-    const [repositorios, setRepositorios] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
     const [loading, setLoading] = useState(false);
 
     function handleinputChange(e) {
@@ -19,18 +19,18 @@ export default function Main() {
 
         async function submit() {
             setLoading(true);
-            try{
+            try {
                 const response = await api.get(`repos/${newRepo}`);
 
                 const data = {
                     name: response.data.full_name,
                 }
-    
+
                 setRepositorios([...repositorios, data]);
                 setNewRepo('');
-            }catch(error){
+            } catch (error) {
                 console.log(error);
-            }finally{
+            } finally {
                 setLoading(false);
             }
 
@@ -38,6 +38,11 @@ export default function Main() {
 
         submit();
     }, [newRepo, repositorios]);
+
+    const handleDelete = useCallback((repo) => {
+        const find = repositorios.filter(r => r.name !== repo)
+        setRepositorios(find);
+    }, [repositorios]);
 
     return (
         <Container>
@@ -59,9 +64,26 @@ export default function Main() {
                         <FaSpinner color='#FFF' size={14} />
                     ) : (
                         <FaPlus size={14} color='#FFF' />
-                    )}                   
+                    )}
                 </SubmitButton>
             </Form>
+
+            <List>
+                {repositorios.map(repo => (
+                    <li key={repo.name}>
+                        <span>
+                            <DeleteButton onClick={()=> handleDelete(repo.name)}>
+                                <FaTrash size={14} />
+                            </DeleteButton>
+                            {repo.name}
+                        </span>
+                        <a href="#">
+                            <FaBars size={20} />
+                        </a>
+                    </li>
+                ))}
+            </List>
+
         </Container>
 
     )
