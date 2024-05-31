acc46f4bb96591ec39f79b77225b337899895151
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-05-31 11:47:11 -0300

    Adicionando animação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, { useState, useCallback } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from '../../styles';
 
+import api from '../../services/api';
+
 export default function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    function handleinputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault(); //// NAO DA REFRESH NA PAGINA AO CLICAR NO BOTAO
+
+        async function submit() {
+            setLoading(true);
+            try{
+                const response = await api.get(`repos/${newRepo}`);
+
+                const data = {
+                    name: response.data.full_name,
+                }
+    
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+            }catch(error){
+                console.log(error);
+            }finally{
+                setLoading(false);
+            }
+
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
     return (
         <Container>
             <h1>
@@ -10,11 +46,20 @@ export default function Main() {
                 Meus Repositórios
             </h1>
 
-            <Form onSubmit={() => { }} >
-                <input type="text" placeholder='Adicionar Repositórios' />
+            <Form onSubmit={handleSubmit} >
+                <input
+                    type="text"
+                    placeholder='Adicionar Repositórios'
+                    value={newRepo}
+                    onChange={handleinputChange}
+                />
 
-                <SubmitButton>
-                    <FaPlus size={14} color='#FFF' />
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {loading ? (
+                        <FaSpinner color='#FFF' size={14} />
+                    ) : (
+                        <FaPlus size={14} color='#FFF' />
+                    )}                   
                 </SubmitButton>
             </Form>
         </Container>
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {keyframes, css} from "styled-components";
 
 export const Container = styled.div`
     max-width: 700px;
@@ -35,7 +35,22 @@ export const Form = styled.form`
     }
 `;
 
-export const SubmitButton = styled.button`
+///Criando animação do botão
+
+const animate = keyframes`
+    from{
+        transform: rotate(0deg);
+    }
+
+    to{
+        transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: "submit",
+    disabled: props.loading,
+}))`
     background: #0D2636;
     border: 0;
     border-radius: 4px;
@@ -44,5 +59,17 @@ export const SubmitButton = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
-    cursor: pointer;
+
+    &[disabled]{
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+
+    ${props => props.loading &&
+    css`
+    svg{
+        animation: ${animate} 2s linear infinite;
+    }
+    `
+    }
 `;
