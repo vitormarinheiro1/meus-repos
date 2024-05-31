c7480c1a5d1b8797d89f0605bd010877c5351fc9
Vitor <vitormarinheiro9@gmail.com>
2024-05-30 23:13:24 -0300

    Criação dos estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AppRoutes from "./routes";
+import GlobalStyle from "./styles/global"
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <AppRoutes />
-    </div>
+    </>
   );
 }
 
